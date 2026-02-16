44bec696eda6d725329e9c1da914441c15660f91
zhiyongliu <1351493417@qq.com>
2026-02-16 18:45:01 +0800

    Task 24: 登录与注册页面 - completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './components/ui/Toast';
 import Layout from './components/layout/Layout';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 import HomePage from './pages/HomePage';
 import ArticlePage from './pages/ArticlePage';
 import CreateArticlePage from './pages/CreateArticlePage';
@@ -8,25 +10,31 @@ import EditArticlePage from './pages/EditArticlePage';
 import CategoryPage from './pages/CategoryPage';
 import TagPage from './pages/TagPage';
 import SearchPage from './pages/SearchPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <ToastProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/article/:slug" element={<ArticlePage />} />
-            <Route path="/create" element={<CreateArticlePage />} />
-            <Route path="/edit/:slug" element={<EditArticlePage />} />
-            <Route path="/category/:slug" element={<CategoryPage />} />
-            <Route path="/tag/:slug" element={<TagPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Layout>
-      </ToastProvider>
+      <AuthProvider>
+        <ToastProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/article/:slug" element={<ArticlePage />} />
+              <Route path="/create" element={<ProtectedRoute requireApproved><CreateArticlePage /></ProtectedRoute>} />
+              <Route path="/edit/:slug" element={<ProtectedRoute requireApproved><EditArticlePage /></ProtectedRoute>} />
+              <Route path="/category/:slug" element={<CategoryPage />} />
+              <Route path="/tag/:slug" element={<TagPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Layout>
+        </ToastProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
